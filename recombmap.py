8716ee4d3b296d45928d4dd3afc72f2d92af7aca
Arvind Narayanan <arvindn@cs.princeton.edu>
2009-07-20 07:57:40 +0000

    blah

---
+++ recombmap.py
@@ -0,0 +1,9 @@
+for chr in xrange(1,24): 
+	pylab.scatter([float(l[-1]) for l in lines if int(l[0]) == chr], [float(l[-2]) for l in lines if int(l[0]) == chr])
+	time.sleep(2)
+	pylab.close()
+
+lines = open('../data/recombination-map.csv') | pTail(skip=30) | pGrep('NA', invert=True) | pSplit(',') 
+
+chr1 = [utils.Struct(pos=int(l[3]), gd=float(l[4]), rr=float(l[7])) for l in lines if l[0] == '1']
+
