411352b5c9e9075a74c66aed84914f82ce7a1646
Paul Ellenbogen <pe5@cs.princeton.edu>
2015-09-25 17:29:35 -0400

    Have adversary return set of possible candidates. Change the number of generations candidate could come from.

--- predict/adversary.py
+++ predict/adversary.py
@@ -5,6 +5,7 @@ import numpy as np
 
 class SimpleAdversary():
     def __init__(self, population, percent = 0.01, generations = 3):
+        self._generations = 3
         self._population = population
         self._individuals = []
         for generation in population.generations[-generations:]:
@@ -17,15 +18,17 @@ class SimpleAdversary():
         target_vector = np.array([kinship[target, person] for person
                                   in self._individuals])
         kinship_error = dict()
-        for potential in self._population.members:
+        potentials = (generation.members for generation
+                      in self._population.generations[-self._generations:])
+        for potential in potentials:
             potential_vector = np.array([kinship[potential, person] for person
                                          in self._individuals])
-            if potential is target:
-                pdb.set_trace()
             sqr_error = (target_vector - potential_vector) ** 2
             kinship_error[potential] = np.sum(sqr_error)
 
-        return min((item[1], item[0]) for item in kinship_error.items())[1]
+        # Return a set because siblings will have the same error.
+        return set(person for person, error in kinship_error.items()
+                   if error == 0.0)
             
         
         
