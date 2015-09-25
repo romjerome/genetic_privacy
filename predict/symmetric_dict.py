5e9fcbf3fbc8288ad8d7d637d30102df7d50c10d
Paul Ellenbogen <pe5@cs.princeton.edu>
2015-09-25 17:27:41 -0400

    Add delete and contains operators to SymmetricDict.

--- predict/symmetric_dict.py
+++ predict/symmetric_dict.py
@@ -1,4 +1,5 @@
 class SymmetricDict(dict):
+
     def __getitem__(self, key):
         return dict.__getitem__(self,
                                 key if key[0] < key[1] else (key[1], key[0]))
@@ -6,3 +7,9 @@ class SymmetricDict(dict):
     def __setitem__(self, key, value):
         dict.__setitem__(self, key if key[0] < key[1] else (key[1],key[0]),
                          value)
+
+    def __delitem__(self, key):
+        dict.__delitem__(self, key if key[0] < key[1] else (key[1],key[0]))
+
+    def __contains__(self, key):
+        return dict.__contains__(self, key if key[0] < key[1] else (key[1],key[0]))
