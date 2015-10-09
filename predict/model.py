f474f74ad33483cb7743204e3e3ddb1075253605
Paul Ellenbogen <pe5@cs.princeton.edu>
2015-10-09 17:30:32 -0400

    Start on alternate ways to generate population.

--- predict/model.py
+++ predict/model.py
@@ -7,3 +7,12 @@ def generate_population(size):
     initial_generation = Generation(Node() for _ in range(size))
     return Population(initial_generation)
 
+class PopulationGenerator():
+    pass
+
+
+class HIMPopulationGeneration(PopulationGenerator):
+    """
+    Generate a population based on the a hierarchical island model.
+    """
+    def __init__(self, num_islands)
