190a85c4ffa7e5104e3956a0a1fb94bbd7e2ca3f
Paul Ellenbogen <pe5@cs.princeton.edu>
2015-12-22 12:18:08 -0500

    Change function name. Finish get_sample_of_cousins implementation.

--- predict/util.py
+++ predict/util.py
@@ -1,5 +1,6 @@
-from collections import deque
+from itertools import combinations, product, chain
 from random import sample
+
 
 def get_sample_of_cousins(population, distance, percent_ancestors = 0.1):
     """
@@ -7,10 +8,16 @@ def get_sample_of_cousins(population, distance, percent_ancestors = 0.1):
     exactly generations back.
     """
     assert distance > 0
-    common_ancestors = population.generations[-(distance + 1)]
+    common_ancestors = population.generations[-(distance + 1)].members
+    last_generation = population.generations[-1].members
     ancestors_sample = sample(common_ancestors,
                               int(len(common_ancestors) * percent_ancestors))
-    descendants = set()
+    pairs = []
+    for ancestor in ancestors_sample:
+        temp_pairs = descendants_with_common_ancestor(ancestor, last_generation)
+        temp_pairs = list(temp_pairs)
+        pairs.extend(sample(temp_pairs, int(len(temp_pairs) * 0.1)))
+    return pairs
 
 def descendants_of(node):
     descendants = set()
@@ -21,7 +28,7 @@ def descendants_of(node):
         to_visit.extend(ancestor.children)
     return descendants
 
-def get_n_degree_relations(ancestor, generation_members):
+def descendants_with_common_ancestor(ancestor, generation_members):
     """
     Returns pairs of individuals descendent from ancestor in the given
     generation who have ancestor as their most recent ancestor.
@@ -32,4 +39,12 @@ def get_n_degree_relations(ancestor, generation_members):
     assert len(ancestor_children) > 1
     descendant_sets = [descendants_of(child).intersection(generation_members)
                        for child in ancestor_children]    
-    
+    pair_iterables = []
+    for descendants_a , descendants_b in combinations(descendant_sets, 2):
+        intersection = descendants_a.intersection(descendants_b)
+        if len(intersection) > 0:
+            # Remove individuals who have a more recent common ancestor
+            descendants_a = descendants_a - intersection
+            descendants_b = descendants_b - intersection
+        pair_iterables.append(product(descendants_a, descendants_b))
+    return chain.from_iterable(pair_iterables)
