cda8a75a787998ff70a04499c98473f41e0b9358
Paul Ellenbogen <pe5@cs.princeton.edu>
2016-04-12 14:18:27 -0400

    Clean up parallelization code.

--- predict/classify_relationship.py
+++ predict/classify_relationship.py
@@ -30,55 +30,43 @@ class LengthClassifier:
                  recombinators, min_segment_length = 0):
         self._distributions = dict()
         labeled_nodes = set(labeled_nodes)
-        labeled_nodes_l = list(labeled_nodes)
         unlabeled_nodes = chain.from_iterable(generation.members
                                               for generation
                                               in population.generations[-3:])
         unlabeled_nodes = set(unlabeled_nodes) - labeled_nodes
-        con = self._set_up_sqlite()
+        con = _set_up_sqlite()
         cur = con.cursor()
-        parallel_func = partial(_parallel_pairwise,
+
+        # Setup for parallelism
+        partitioned_labeled = _partition_labeled_nodes(labeled_nodes)
+        parallel_shared = partial(_pairwise_shared,
                                 unlabeled_nodes = unlabeled_nodes,
                                 min_segment_length = min_segment_length)
                                 
-        for i in range(3):
+        for i in range(10):
             print("iteration {}".format(i))
             print("Cleaning genomes.")
             population.clean_genomes()
             print("Generating genomes")
             generate_genomes(population, genome_generator, recombinators, 3)
             print("Calculating shared length")
-            partition_size = len(labeled_nodes_l) // (NUM_CPU * 2)
-            chunked_labeled = [labeled_nodes_l[i:i+ partition_size] for i
-                               in range(0, len(labeled_nodes_l),
-                                        partition_size)]
             with Pool(NUM_CPU) as p:
-                lengths_iter = chain.from_iterable(p.imap(parallel_func,
-                                                          chunked_labeled))
+                lengths_iter = chain.from_iterable(p.imap(parallel_shared,
+                                                          partitioned_labeled))
                 cur.executemany("INSERT INTO lengths VALUES (?, ?, ?)",
                                 lengths_iter)
             con.commit()
-        # print("Generating distributions")
-        # for unlabeled, labeled in product(unlabeled_nodes, labeled_nodes):
-        #     query = cur.execute("""SELECT shared
-        #                            FROM lengths
-        #                            WHERE unlabeled = ? AND labeled = ?""",
-        #                         (unlabeled._id, labeled._id))
-        #     lengths = [value[0] for value in query]
-        #     self._distributions[unlabeled, labeled] = gamma(*gamma.fit(lengths))
+        print("Generating distributions")
+        for unlabeled, labeled in product(unlabeled_nodes, labeled_nodes):
+            query = cur.execute("""SELECT shared
+                                   FROM lengths
+                                   WHERE unlabeled = ? AND labeled = ?""",
+                                (unlabeled._id, labeled._id))
+            lengths = [value[0] for value in query]
+            self._distributions[unlabeled, labeled] = gamma(*gamma.fit(lengths))
         cur.close()
             
-    def _set_up_sqlite(self):
-        if isfile(DB_FILE): # Clear old versions of this file
-            remove(DB_FILE)
-        temp_storage = sqlite3.connect(DB_FILE)
-        temp_storage.execute("""PRAGMA page_size = 32768""");
-        temp_storage.execute("""CREATE TABLE lengths
-                                (unlabeled integer, labeled integer, shared integer)""")
-        temp_storage.execute("""CREATE INDEX labeled_index
-                                ON lengths (labeled)""")
-        temp_storage.commit()
-        return temp_storage
+
 
             
     def get_probability(self, shared_length, query_node, labeled_node):
@@ -88,13 +76,30 @@ class LengthClassifier:
         """
         return self._distributions[query_node, labeled_node].pdf(shared_length)
 
-def _parallel_pairwise(labeled_nodes, unlabeled_nodes, min_segment_length):
+def _set_up_sqlite():
+    if isfile(DB_FILE): # Clear old versions of this file
+        remove(DB_FILE)
+    temp_storage = sqlite3.connect(DB_FILE)
+    temp_storage.execute("""PRAGMA page_size = 32768""");
+    temp_storage.execute("""CREATE TABLE lengths
+                            (unlabeled integer, labeled integer, shared integer)""")
+    temp_storage.execute("""CREATE INDEX labeled_index
+                            ON lengths (labeled)""")
+    temp_storage.commit()
+    return temp_storage
+
+def _pairwise_shared(labeled_nodes, unlabeled_nodes, min_segment_length):
     return [(unlabeled._id, labeled._id,
              shared_segment_length_genomes(unlabeled.genome,
                                            labeled.genome,
                                            min_segment_length))
             for unlabeled, labeled
             in product(unlabeled_nodes, labeled_nodes)]
+
+def _partition_labeled_nodes(labeled_nodes):
+    l = list(labeled_nodes)
+    partition_size = len(l) // (NUM_CPU * 2)
+    return [l[i:i+ partition_size] for i in range(0, len(l), partition_size)]
     
 def shared_segment_length_genomes(genome_a, genome_b, minimum_length):
     by_autosome = common_segment_lengths(genome_a, genome_b)
