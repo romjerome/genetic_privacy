167200dfefdb723d04d5c11e1510cb2af188cfa1
Paul Ellenbogen <pe5@cs.princeton.edu>
2016-04-13 17:28:58 -0400

    Fix gamma fitting errors when there are 0 valued elements.

--- predict/gamma.py
+++ predict/gamma.py
@@ -10,18 +10,19 @@ def fit_gamma(data):
     http://research.microsoft.com/en-us/um/people/minka/papers/minka-gamma.pdf
     Returns a tuple with (shape, scale) parameters.
     """
+    data += 1e-8 # Add small number to avoid 0s in the data causing issues.
     log_of_mean = log(np.mean(data))
     mean_of_logs = np.mean(np.log(data))
     log_diff = mean_of_logs - log_of_mean
-    a = 0.5 / (log_of_mean - mean_of_logs)
-    a_reciprocal = 1/a
+    shape = 0.5 / (log_of_mean - mean_of_logs)
+    shape_reciprocal = 1  / shape
     difference = 1
     while difference > 0.000005:
-        numerator = log_diff + log(a) - digamma(a)
-        denominator = (a ** 2) * (a_reciprocal - polygamma(1, a))
-        tmp_a_reciprocal = a_reciprocal + numerator / denominator
-        tmp_a = 1 / tmp_a_reciprocal
-        difference = abs(tmp_a - a)
-        a = tmp_a
-        a_reciprocal = tmp_a_reciprocal
-    return (a, np.mean(data) / a)
+        numerator = log_diff + log(shape) - digamma(shape)
+        denominator = (shape ** 2) * (shape_reciprocal - polygamma(1, shape))
+        tmp_shape_reciprocal = shape_reciprocal + numerator / denominator
+        tmp_shape = 1 / tmp_shape_reciprocal
+        difference = abs(tmp_shape - shape)
+        shape = tmp_shape
+        shape_reciprocal = tmp_shape_reciprocal
+    return (shape, np.mean(data) / shape)
