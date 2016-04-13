6fea965fea422361997769e4328b7f4c6242941e
Paul Ellenbogen <pe5@cs.princeton.edu>
2016-04-13 15:59:43 -0400

    Add module for fitting gamma distributions faster.

---
+++ predict/gamma.pyx
@@ -0,0 +1,29 @@
+# from math import log # Python's built in log is faster for non vector operations
+from libc.math cimport log
+
+import numpy as np
+from scipy.special import digamma, polygamma
+
+INF = float("inf")
+
+def fit_gamma(data):
+    """
+    Gamma parameter estimation using the algorithm described in
+    http://research.microsoft.com/en-us/um/people/minka/papers/minka-gamma.pdf
+    """
+    cdef float log_of_mean, mean_of_logs, log_diff, a, a_reciprocal, difference
+    log_of_mean = log(np.mean(data))
+    mean_of_logs = np.mean(np.log(data))
+    log_diff = mean_of_logs - log_of_mean
+    a = 0.5 / (log_of_mean - mean_of_logs)
+    a_reciprocal = 1/a
+    difference = 1
+    while difference > 0.000005:
+        numerator = log_diff + log(a) - digamma(a)
+        denominator = (a ** 2) * (a_reciprocal - polygamma(1, a))
+        tmp_a_reciprocal = a_reciprocal + numerator / denominator
+        tmp_a = 1 / tmp_a_reciprocal
+        difference = abs(tmp_a - a)
+        a = tmp_a
+        a_reciprocal = tmp_a_reciprocal
+    return (a, np.mean(data) / a)
