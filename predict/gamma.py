db27ef6f096dda22bc1c9cbbae115a44b2a264c8
Paul Ellenbogen <pe5@cs.princeton.edu>
2016-04-18 18:17:51 -0400

    Compute mean only once.

--- predict/gamma.py
+++ predict/gamma.py
@@ -11,7 +11,8 @@ def fit_gamma(data):
     Returns a tuple with (shape, scale) parameters.
     """
     data += 1e-8 # Add small number to avoid 0s in the data causing issues.
-    log_of_mean = log(np.mean(data))
+    data_mean = np.mean(data)
+    log_of_mean = log(data_mean)
     mean_of_logs = np.mean(np.log(data))
     log_diff = mean_of_logs - log_of_mean
     shape = 0.5 / (log_of_mean - mean_of_logs)
@@ -25,4 +26,4 @@ def fit_gamma(data):
         difference = abs(tmp_shape - shape)
         shape = tmp_shape
         shape_reciprocal = tmp_shape_reciprocal
-    return (shape, np.mean(data) / shape)
+    return (shape, data_mean / shape)
