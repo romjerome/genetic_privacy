b7f816c436a86ac230b7760d1e6207456f789ce7
Paul Ellenbogen <pe5@cs.princeton.edu>
2016-04-21 17:44:23 -0400

    Create scripts to run genome comaprison on the cluster.

--- predict/cluster_scripts/calculate_distributions.sh
+++ predict/cluster_scripts/calculate_distributions.sh
@@ -3,11 +3,24 @@ set -euo pipefail
 
 ulimit -Sv 1000000
 
-python3 ../divide_work.py ../10000 10 --num_labeled 100 --output_dir ../test_output
+POPULATION_DIR="$1"
+POPULATION_BASE="$(basename "${POPULATION_DIR}")"
+CODE_DIR="/n/fs/grad/pe5/genetic_privacy/predict"
+SCRATCH_POP_DIR="/scratch/${USER}/${POPULATION_BASE}"
+REMOTE_POP_DIR="/scratch/network/${USER}/${POPULATION_BASE}"
+OUTPUT_DIR="/n/fs/grad/pe5/genetic_privacy_output"
+
+mkdir -p "/scratch/${USER}/db"
+mkdir -p "${OUTPUT_DIR}"
+
+cp -r "${POPULATION_DIR}" "${SCRATCH_POP_DIR}"
+ 
+srun --mem=4GB "${CODE_DIR}/divide_work.py" "${POPULATION_DIR}" 5 \
+     --num_labeled 50 --output_dir "${OUTPUT_DIR}"
 
 for FILENAME in ../test_output/partition_*_labeled_nodes.pickle
 do
-    NUM="$(basename $FILENAME | grep -o -E '[0-9]+')"
-    python3 ../parallel_distributions.py ../10000 \
-            "$FILENAME" ../test_output --database_name "labeled_${NUM}.db"
+    NUM="$(basename "$FILENAME" | grep -o -E '[0-9]+')"
+    srun remote_subset_shared.sh "${REMOTE_POP_DIR}" "$FILENAME" \
+         "${OUTPUT_DIR}" "labeled_${NUM}.db"
 done
