09fc4d1bdbdb1a0f9db14d517fe7daba0347ac57
Paul Ellenbogen <pe5@cs.princeton.edu>
2016-04-26 17:33:41 -0400

    Reintroduce non vector method for finding common ancestors.

--- predict/common_segments.pyx
+++ predict/common_segments.pyx
@@ -1,11 +1,14 @@
-# cython: profile=True
+"""# cython: profile=True"""
 import numpy as np
 from array import array
 
 cimport numpy as np
 
 
-def common_segment_lengths(genome_a, genome_b):
+def common_segment_lengths_bit_vector(genome_a, genome_b):
+    """
+    Compute common segment lengths using bit vectors.
+    """
     cdef unsigned long[:] a_m_starts, a_m_stops, a_f_starts, a_f_stops
     cdef unsigned long[:] b_m_starts, b_m_stops, b_f_starts, b_f_stops
     cdef unsigned long common_founder
@@ -106,6 +109,75 @@ cdef list overlapping_regions(unsigned long[:] starts_a, unsigned long[:] stops_
     # (0, 5) and (5, 10), then we should merge them into (0, 10).
     # XXX: This now consolidates across chromosmoes, which may be undesirable
     return _consolidate_sequence(shared_segments)
+
+### Non vectorized method for calculating common segment lengths.
+
+def common_segment_lengths(genome_a, genome_b):
+    """
+    Given two genomes returns a list of integers for each autosome,
+    corresponding to the length of segments that are shared between
+    the two autosomes.
+    """
+    cdef list lengths = []
+    lengths.extend(_lengths(common_homolog_segments(genome_a.mother,
+                                                    genome_b.mother)))
+    lengths.extend(_lengths(common_homolog_segments(genome_a.father,
+                                                    genome_b.mother)))
+    lengths.extend(_lengths(common_homolog_segments(genome_a.mother,
+                                                    genome_b.father)))
+    lengths.extend(_lengths(common_homolog_segments(genome_a.father,
+                                                    genome_b.father)))
+    return lengths
+
+cpdef list common_homolog_segments(homolog_a, homolog_b):
+    """
+    Given two autosome homologs, returns a list of ranges (a, b), (b, c), ...
+    where the two autosomes have the same underlying sequence.
+    """
+    cdef unsigned long len_a, len_b, index_a, index_b, start, stop
+    cdef unsigned long a_start, a_stop, a_id, b_start, b_stop, b_id
+    cdef unsigned long[:] starts_a = homolog_a.starts
+    cdef unsigned long[:] stops_a = homolog_a.stops
+    cdef unsigned long[:] founder_a = homolog_a.founder
+    cdef unsigned long[:] starts_b = homolog_b.starts
+    cdef unsigned long[:] stops_b = homolog_b.stops
+    cdef unsigned long[:] founder_b = homolog_b.founder
+    len_a = len(starts_a)
+    len_b = len(starts_b)
+    index_a = 0
+    index_b = 0
+    cdef list shared_segments = []
+    while index_a < len_a and index_b < len_b:        
+        a_start, a_stop = starts_a[index_a], stops_a[index_a]
+        a_id = founder_a[index_a]
+        b_start, b_stop = starts_b[index_b], stops_b[index_b]
+        assert a_start < a_stop
+        assert b_start < b_stop
+        b_id = founder_b[index_b]
+        if a_id == b_id:
+            if a_start > b_start:
+                start = a_start
+            else:
+                start = b_start
+            if a_stop < b_stop:
+                stop = a_stop
+            else:
+                stop = b_stop            
+            shared_segments.append((start, stop))
+        if a_stop == b_stop:
+            index_a += 1
+            index_b += 1
+        elif a_stop > b_stop:
+            index_b += 1
+        else:
+            index_a += 1
+    if len(shared_segments) <= 1:
+        return shared_segments
+    # consolidate contiguous segments eg if we have shared segments
+    # (0, 5) and (5, 10), then we should merge them into (0, 10).
+    return _consolidate_sequence(shared_segments)
+
+
 
 cdef list _lengths(list segments):
     """
