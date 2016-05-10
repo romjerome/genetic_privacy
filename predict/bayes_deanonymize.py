6a8902ce5f3426cf884b3334eedde5c5e9b006ed
Paul Ellenbogen <pe5@cs.princeton.edu>
2016-05-10 17:28:48 -0400

    Add cache to store shared segment length between labeled node and genome in question.

--- predict/bayes_deanonymize.py
+++ predict/bayes_deanonymize.py
@@ -15,27 +15,36 @@ class BayesDeanonymize:
         else:
             self._length_classifier = classifier
 
-    def _compare_genome_node(self, node, genome):
+    def _compare_genome_node(self, node, genome, cache):
         probabilities = []
         length_classifier = self._length_classifier
         for labeled_node in length_classifier._labeled_nodes:
             if (node, labeled_node) not in length_classifier:
                 continue
-            shared = shared_segment_length_genomes(genome, labeled_node.genome,
-                                                   0)
-            prob = length_classifier.get_probability(shared,
-                                                     node,
+            if labeled_node in cache:
+                shared = cache[labeled_node]
+            else:
+                shared = shared_segment_length_genomes(genome,
+                                                       labeled_node.genome,
+                                                       0)
+                cache[labeled_node] = shared
+                
+            prob = length_classifier.get_probability(shared, node,
                                                      labeled_node)
+                
             probabilities.append(prob)
+            
         return list(filter(lambda x: x is not None, probabilities))
 
         
     def identify(self, genome):
         node_probabilities = dict() # Probability that a node is a match
+        shared_genome_cache = dict()
         for member in self._population.members:
             if member.genome is None:
                 continue
-            probabilities = self._compare_genome_node(member, genome)
+            probabilities = self._compare_genome_node(member, genome,
+                                                      shared_genome_cache)
             if len(probabilities) < MINIMUM_LABELED_NODES:
                 # We don't want to base our estimation on datapoints
                 # from too few labeled nodes.
