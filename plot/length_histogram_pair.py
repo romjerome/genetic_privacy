139ba0af5dbca05cf2b39f77d295610ff3feb681
Paul Ellenbogen <pe5@cs.princeton.edu>
2016-05-21 16:15:18 -0400

    Change relationship plotter.

--- plot/length_histogram_pair.py
+++ plot/length_histogram_pair.py
@@ -2,11 +2,14 @@ from random import choice
 from collections import defaultdict
 
 import matplotlib.pyplot as plt
+# from matplotlib import rc, rcParams
 import numpy as np
+from scipy.interpolate import UnivariateSpline
 
 from scipy.stats import beta, gamma, poisson, exponweib
+LENGTHS_FILENAME = "/media/paul/Storage/scratch/lengths/70004"
 
-LENGTHS_FILENAME = "lengths/70004"
+# rcParams.update({'font.size': 24})
 
 def get_lengths(lengths_filename):
     lengths = defaultdict(list)
@@ -22,34 +25,68 @@ def get_lengths(lengths_filename):
                 continue
             lengths[node_id].append(length)
     return lengths
+
+
+def multi_probs(lengths_list, labels = None):
+    for i, lengths in enumerate(lengths_list):
+        lengths.sort()
+        prob, x = np.histogram(lengths, bins = 15)
+        x = x[:-1] + (x[1] - x[0])/2   # convert bin edges to centers
+        f = UnivariateSpline(x, prob)
+        if labels is not None:
+            plt.plot(x, f(x), label = labels[i])
+        else:
+            plt.plot(x, f(x))
+    if labels is not None:
+        plt.legend(loc = "upper right")
+    plt.ylabel("Simliarity function probability", size = 20)
+    plt.xlabel("Similarity function value", size = 20)
+    plt.show()
+
+def multi_probs_2(lengths_list, labels = None):
+    for i, lengths in enumerate(lengths_list):
+        lengths.sort()
+        weights = np.ones_like(lengths)/float(len(lengths))
+        if labels is not None:
+            plt.hist(lengths, bins = 20, weights = weights,
+                     alpha = 0.5, normed = False,
+                     label = labels[i])
+        else:
+            plt.hist(lengths, bins = 20, weights = weights,
+                     alpha = 0.5, normed = False)
+    if labels is not None:
+        plt.legend(loc = "upper right")
+    plt.ylabel("Simliarity function probability", size = 20)
+    plt.xlabel("Similarity function value", size = 20)
+    plt.show()
             
     
 def plot_histogram(lengths):
     lengths.sort()
-    weights = np.ones_like(lengths)/float(len(lengths))
-    plt.hist(lengths, weights=weights, bins = 20)
+    # weights = np.ones_like(lengths)/float(len(lengths))
+    # plt.hist(lengths, weights=weights, bins = 20)
+    prob, x = np.histogram(lengths, bins = 15)
+    x = x[:-1] + (x[1] - x[0])/2   # convert bin edges to centers
+    f = UnivariateSpline(x, prob)
+    plt.plot(x, f(x))
+    
+    plt.ylabel("Simliarity function probability", size = 20)
+    plt.xlabel("Similarity function value", size = 20)
+    # plt.hist(lengths, normed = True, bins = 20)
 
-    np_lengths = np.array(lengths)
-    beta_pdf = beta(*beta.fit(np_lengths)).pdf(np_lengths)
-    gamma_pdf = gamma(*gamma.fit(np_lengths)).pdf(np_lengths)
-    exponweib_pdf = exponweib(*exponweib.fit(np_lengths)).pdf(np_lengths)
+    # np_lengths = np.array(lengths)
+    # beta_pdf = beta(*beta.fit(np_lengths)).pdf(np_lengths)
+    # gamma_pdf = gamma(*gamma.fit(np_lengths)).pdf(np_lengths)
 
-    beta_plot = plt.plot(lengths, beta_pdf, label = "Beta")
-    gamma_plot = plt.plot(lengths, gamma_pdf, label = "Gamma")
-    weib_plot = plt.plot(lengths, exponweib_pdf, label = "Exp Weib")
-    plt.legend(loc = "upper right")
+    # beta_plot = plt.plot(lengths, beta_pdf, label = "Beta")
+    # gamma_plot = plt.plot(lengths, gamma_pdf, label = "Gamma")
+    # plt.legend(loc = "upper right")
     plt.show()
 
 lengths_vectors = get_lengths(LENGTHS_FILENAME)
-for node_id, lengths in lengths_vectors.items():
-    plot_histogram(lengths)
-
-for unlabeled, labeled in nonzero_pairs:
-    lengths = get_lengths(unlabeled, labeled, c)
-    if np.var(lengths) < 100:
-        continue
-    import pdb
-    # pdb.set_trace()
-    plot_histogram(lengths)
+# plot_histogram(lengths_vectors[77850])
+multi_probs_2([lengths_vectors[77850], lengths_vectors[70003]],
+            ["first cousin", "full sibling"])
+# for node_id, lengths in lengths_vectors.items():
+#     plot_histogram(lengths)
     
-conn.close()
